5afddfd97d75dbc37ee8cca7f6bb425254595ac2
OlivieroPinto <pintooliviero@gmail.com>
2022-06-14 17:28:31 +0200

    inserimento e ottimizzazoione bonus

--- src/jana60/geometria/Main.java
+++ src/jana60/geometria/Main.java
@@ -5,15 +5,31 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		// int area[] = new int[5];
+		int numeromassimo = 0;
 		Rettangolo lati = new Rettangolo();
 		System.out.println("Inserisci lati del rettangolo:");
-		Scanner scan = new Scanner(System.in);
-		lati.base = scan.nextInt();
-		lati.altezza = scan.nextInt();
-		System.out.println("l'area del rettangolo é: " + lati.calcoloArea(lati.base, lati.altezza));
-		System.out.println("mentre il perimetro é: " + lati.calcoloPerimetro(lati.base, lati.altezza));
-		scan.close();
+		for (int i = 0; i < 5; i++) {
+			Scanner scan = new Scanner(System.in);
+			lati.base = scan.nextInt();
+			lati.altezza = scan.nextInt();
+			if (lati.calcoloArea(lati.base, lati.altezza) >= numeromassimo) {
+				numeromassimo = lati.calcoloArea(lati.base, lati.altezza);
+			}
+			// area[i] = lati.calcoloArea(lati.base, lati.altezza);
+			// System.out.println(area[i]);
+		}
+		// System.out.println("l'area del rettangolo é: " + lati.calcoloArea(lati.base,
+		// lati.altezza));
+		// System.out.println("mentre il perimetro é: " +
+		// lati.calcoloPerimetro(lati.base, lati.altezza));
+		// scan.close();
 
+		// for (int i = 0; i < 5; i++) {
+		// if (area[i] > numeromassimo) {
+		// numeromassimo = area[i];
+		// }
+		// }
+		System.out.println("il rettangolo piu grande ha area di " + numeromassimo);
 	}
-
 }
